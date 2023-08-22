ffb9e61d9a911eae41b1269d1bc2cc3163a723a3
dieniferverch <142456047+dieniferverch@users.noreply.github.com>
2023-08-21 21:43:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+dienifer
+* Primeiro comentario
+*/
+console.log("olá mundo");
+//prompt("digite seu nome");
+
+//variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Dienifer";
+const idade = 31;
+
+console.log("olá!", "Meu nome é ", nome, "e eu tenho", idade, "anos.");
+
+
+let nomeExercicio = "Dienifer";
+let sobrenome = "verch";
+let minhaIdade = 31;
+let souEstudante = true;
+
+console.log("olá!", "Meu nome é ", nomeExercicio, sobrenome,"e eu tenho", minhaIdade, "anos.", souEstudante);
